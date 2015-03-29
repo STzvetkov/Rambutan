8f840e51dd8bbb785157274806c03bcfa3a3c9ee
Simeon Tzvetkov <simeon_tzvetkov@mail.bg>
2015-03-29 12:52:08 +0300

    Zoo management

---
+++ Rambutan/ZooManagement.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zoo.Animals;
+using Zoo.Employees;
+using Zoo.BudgetInfo;
+
+namespace Zoo
+{
+    public class ZooManagement
+    {
+        public ZooManagement()
+        {
+            this.AnimalsDB = new List<Animal>();
+        }
+
+        // Stores all employees
+        public List<Employee> StaffDB { get; private set; }
+        
+
+
+        // Stores all animals in the zoo
+        public List<Animal> AnimalsDB { get; private set; }
+        
+        public void HireEmployee(Employee employee)
+        {
+            this.StaffDB.Add(employee);
+            //Budget.Instance().AddExpense(employee.Position, employee.);
+        }
+
+        public void DischargeEmployee(long employeeID)
+        {
+            Employee employToBeDischarged = this.StaffDB.Find(x => x.StaffID == employeeID);
+            this.StaffDB.Remove(employToBeDischarged);
+        }
+
+        public void BuyAnimal(Animal animal)
+        {
+            this.AnimalsDB.Add(animal);
+            //Budget.Instance().AddExpense(animal.type, animal.cost);
+        }
+
+        public void SellAnimal(long animalID)
+        {
+        }
+    }
+}
