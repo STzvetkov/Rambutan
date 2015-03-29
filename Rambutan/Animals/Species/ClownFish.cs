8493241fd5665ba1c58ec5449adc723826133c0b
petyakostova <petya_kostova_@abv.bg>
2015-03-29 17:56:12 +0300

    add Dolphin.cs and ClownFish.cs (from class Aquatic)

--- Rambutan/Animals/Species/ClownFish.cs
+++ Rambutan/Animals/Species/ClownFish.cs
@@ -1,12 +1,12 @@
 ﻿namespace Zoo.Animals.Species
 {
     using System;
-
     using Zoo.Animals.Type;
     using Zoo.Interfaces;
 
     public class ClownFish : Aquatic, IOmnivore, IFeedable
     {
         // TODO : Implement all methods and fields needed.
+        // food: algae, plankton, crustaceans
     }
 }
