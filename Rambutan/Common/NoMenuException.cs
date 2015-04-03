a78f69827288d3585743ae8d3c6ffdd00127db35
Martin <mr.krustev1994@gmail.com>
2015-04-03 18:43:57 +0300

    MenuException

---
+++ Rambutan/Common/NoMenuException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Common
+{
+    public class NoMenuException : Exception
+    {
+        public NoMenuException(string msg)
+            :base(msg)
+        {
+        }
+    }
+}
