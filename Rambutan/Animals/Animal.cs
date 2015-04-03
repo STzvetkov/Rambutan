ff7acfe73371681ea1a12cc8463630914b8bd9f7
petyakostova <petya_kostova_@abv.bg>
2015-04-03 03:42:26 +0300

    modify Animal.cs => added field price and properties: Gender, Age, Price, DietType, ArrivalDate, Cage, Habitat, HealthStatus, ExaminedBy

--- Rambutan/Animals/Animal.cs
+++ Rambutan/Animals/Animal.cs
@@ -20,39 +20,42 @@
         Python,
         Rabbit
     }
-    public abstract class Animal:IFeedable
+
+    public abstract class Animal : IFeedable
     {
         // abstract information/methods that all animals can have.
 
         private long animalID;
 
-        private AnimalType type; 
+        private AnimalType type; // AnimalType enumeration
         
-        private Gender gender;  // Gender enumeration
+        private Gender gender;   // Gender enumeration
 
         private int age;
 
+        private decimal price;
+
         private string dietType;
 
-        private double height;
+        //private double height;
 
-        private double weight;
+        //private double weight;
 
-        private string color;
+        //private string color;
 
-        private string description;       
+        //private string description;       
         
         private DateTime arrivalDate;
 
-        private Cage cage;  // TODO: implement Cage
+        private Cage cage;              // TODO: add number of cage (Animal don't need all information about the cage => ToString()) 
 
-        //private HabitatType habitat;  // TODO: HabitatType enumeration      
+        private HabitatType habitat;    // TODO: HabitatType enumeration      
 
         private string healthStatus;
 
-        private DateTime dateOfLastExamination;
+        //private DateTime dateOfLastExamination;
 
-        private Veterinarian examinedBy;
+        private Veterinarian examinedBy;    // TODO: add name of veterinarian (Animal don't need all information about him => ToString()) 
 
         // constructors
 
@@ -81,7 +84,8 @@
                 if (value == 0)
                 {
                     throw new ArgumentException("AnimalID can not be null!");
-                }                
+                }    
+            
                 this.animalID = value; 
             }
         }
@@ -94,52 +98,166 @@
             }
             set
             {
-                // TODO: Enter checks
                 this.type = value;
             }
         }
 
-        // TODO: implement more properties after corroboration of the fields
+        public Gender Gender
+        {
+            get
+            {
+                return this.gender;
+            }
+            set
+            {
+                this.gender = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            set
+            {
+                // TODO: Enter checks
+                this.age = value;
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                // TODO: Enter checks
+                this.price = value;
+            }
+        }
+
+        public string DietType
+        {
+            get
+            {
+                return this.dietType;
+            }
+            set
+            {
+                // TODO: Enter checks
+                this.dietType = value;
+            }
+        }
+
+        public DateTime ArrivalDate
+        {
+            get
+            {
+                return this.arrivalDate;
+            }
+            set
+            {
+                // TODO: Enter checks
+                this.arrivalDate = value;
+            }
+        }
+
+        public Cage Cage
+        {
+            get
+            {
+                return this.cage;
+            }
+            set
+            {
+                // TODO: Enter checks
+                this.cage = value;
+            }
+        }
+
+        public HabitatType Habitat
+        {
+            get
+            {
+                return this.habitat;
+            }
+            set
+            {
+                this.habitat = value;
+            }
+        }
+
+        public string HealthStatus
+        {
+            get
+            {
+                return this.healthStatus;
+            }
+            set
+            {
+                // TODO: Enter checks
+                this.healthStatus = value;
+            }
+        }
+
+        public Veterinarian ExaminedBy
+        {
+            get
+            {
+                return this.examinedBy;
+            }
+            set
+            {
+                // TODO: Enter checks
+                this.examinedBy = value;
+            }
+        }
+        
+        // TODO: eventually implement more properties after corroboration of the fields
 
         // methods 
 
-        // TODO: modify ToString()
-        public override string ToString()
-        {
-            return string.Format(
+        // TODO: modify ToString() after corroboration of the fields
+//        public override string ToString()
+//        {
+//            return string.Format(
 
-           @"AnimalID : {0} 
-AnimalType: {1}
-Gender: {2}
-DietType: {3}
-Height: {4} 
-Weight: {5}
-Color: {6}
-Description: {7}
-ArrivalDate: {8}
-Cage: {9}
-Habitat: {10}
-HealthStatus: {11}
-DateOfLastExamination: {12}
-ExaminedBy: {13}
-{14}",            
-            this.animalID,
-            //this.type,
-            this.gender,
-            this.dietType,
-            this.height,
-            this.weight,
-            this.color,
-            this.description,        
-            this.arrivalDate,
-            this.cage,
-            //this.habitat,
-            this.healthStatus,
-            this.dateOfLastExamination,
-            this.examinedBy,            
-            new string('-', 40)
-           );
-        }
+//           @"AnimalID : {0} 
+//AnimalType: {1}
+//Gender: {2}
+//DietType: {3}
+//Height: {4} 
+//Weight: {5}
+//Color: {6}
+//Description: {7}
+//ArrivalDate: {8}
+//Cage: {9}
+//Habitat: {10}
+//HealthStatus: {11}
+//DateOfLastExamination: {12}
+//ExaminedBy: {13}
+//{14}",
+//            this.animalID,
+//                //this.type,
+//            this.gender,
+//            this.dietType,
+//                //this.height,
+//                //this.weight,
+//                //this.color,
+//                //this.description,        
+//            this.arrivalDate,
+//            this.cage,
+//                //this.habitat,
+//            this.healthStatus,
+//            this.dateOfLastExamination,
+//            this.examinedBy,
+//            new string('-', 40)
+//           );
+//        }
 
         //TODO: Implement more methods
     }
