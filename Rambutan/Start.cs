0d5e89197fc223057c65697d8ebdd61ebd7c7aa1
Martin <mr.krustev1994@gmail.com>
2015-04-03 18:47:34 +0300

    MenuImplementation

--- Rambutan/Start.cs
+++ Rambutan/Start.cs
@@ -5,10 +5,16 @@
     using Employees;
     using Zoo.Animals;
     using Zoo.Animals.Consumables;
-    using Zoo.Schedules;
+    using Zoo.Schedules;   
+    using System.Drawing;
+    using Zoo.Menu;
 
     class Start
     {
+        static ConsoleKeyInfo cki;
+        public static BaseMenuState state;
+        public static bool programIsRunning = true;
+
         static void Main()
         {
             // <test>
@@ -30,9 +36,113 @@
 
             var schedule = new Schedule("11:15",Zoo.Schedules.Schedule.Frequency.weekly); // kak da se sykrati?
             schedule.AddDay(DayOfWeek.Sunday);
+
+            bool programIsRunning = true;
+
+
+            Console.WindowHeight = 45;
+            Console.BufferHeight = Console.WindowHeight;
+            Console.WindowWidth = 100;
+            Console.BufferWidth = Console.WindowWidth;
             
 
+            #region Menu
+            ChangeMenu(new MainMenu());
 
+            Console.CursorVisible = false;
+
+            while (programIsRunning)
+            {
+                Console.Clear();
+                state.PrintMenu();
+                ReadKey();
+                TakeChoice(cki);
+            }
+            #endregion
+        }
+
+        private static void Print(int row, int col, object data)
+        {
+            Console.SetCursorPosition(col, row);
+            Console.Write(data);
+        }
+
+        private static void ReadKey()
+        {
+            Console.SetCursorPosition(5, 5);
+            cki = Console.ReadKey();
+            Console.SetCursorPosition(5, 5);
+            Console.Write(" ");
+        }
+
+        private static void TakeChoice(ConsoleKeyInfo cki)
+        {
+            switch (cki.Key)
+            {
+                case ConsoleKey.D0:
+                case ConsoleKey.NumPad0:
+                    state.OptionZero();
+                    break;
+
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    if (state.HasOptionOne)
+                    {
+                        ChangeMenu(state.OptionOne);
+                    }
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    if (state.HasOptionTwo)
+                    {
+                        ChangeMenu(state.OptionTwo);
+                    }
+                    break;
+
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    if (state.HasOptionThree)
+                    {
+                        ChangeMenu(state.OptionThree);
+                    }
+                    break;
+
+                case ConsoleKey.D4:
+                case ConsoleKey.NumPad4:
+                    if (state.HasOptionFour)
+                    {
+                        ChangeMenu(state.OptionFour);
+                    }
+                    break;
+
+                case ConsoleKey.D5:
+                case ConsoleKey.NumPad5:
+                    break;
+
+                case ConsoleKey.D6:
+                case ConsoleKey.NumPad6:
+                    break;
+
+                case ConsoleKey.D7:
+                case ConsoleKey.NumPad7:
+                    break;
+
+                case ConsoleKey.D8:
+                case ConsoleKey.NumPad8:
+                    break;
+
+                case ConsoleKey.D9:
+                case ConsoleKey.NumPad9:
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private static void ChangeMenu(BaseMenuState menu)
+        {
+            state = menu;
         }
     }
 }
