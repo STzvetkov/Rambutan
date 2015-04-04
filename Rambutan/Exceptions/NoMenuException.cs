2e9d7db2a64c49315e770aab123a8a65ffd038ab
Martin <mr.krustev1994@gmail.com>
2015-04-04 11:22:34 +0300

    ExceptionsUpdate

--- Rambutan/Exceptions/NoMenuException.cs
+++ Rambutan/Exceptions/NoMenuException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Zoo.Common
+namespace Zoo.Exceptions
 {
     public class NoMenuException : Exception
     {
--- Rambutan/Menu/EstateInformation/MenuFoodAvailable.cs
+++ Rambutan/Menu/EstateInformation/MenuFoodAvailable.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Zoo.Common;
+using Zoo.Exceptions;
 
 namespace Zoo.Menu.EstateInformation
 {
