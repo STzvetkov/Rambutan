753d03d10932e3f11386a74106be91b78f5f44bf
Simeon Tzvetkov <simeon_tzvetkov@mail.bg>
2015-04-04 11:28:38 +0300

    Employee exception

---
+++ Rambutan/Exceptions/EmployeeNotFoundException.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Exceptions
+{
+    class EmployeeNotFoundException:Exception
+    {
+        public EmployeeNotFoundException()
+        {
+        }
+
+        public EmployeeNotFoundException(string message) : base(message)
+        {
+        }
+
+        public EmployeeNotFoundException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
