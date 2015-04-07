1ada647c0315609df9a45410d56a8e965bd82c5d
boytchoteofilov <boytcho.teofilov@gmail.com>
2015-04-07 15:53:52 +0300

    addeed properties to foodstorage

--- Rambutan/Animals/Consumables/FoodStorage.cs
+++ Rambutan/Animals/Consumables/FoodStorage.cs
@@ -11,6 +11,8 @@
         // Fields
         private FoodStorageType typeOfStorage;
         private List<Food> storage;
+        private decimal price;
+        private long storageID;
 
         // Constructor
         public FoodStorage(FoodStorageType type)
@@ -19,6 +21,41 @@
 
         }
 
+        // Properties
+
+        public decimal Price 
+        {
+            get 
+            {
+                return this.price; 
+            }
+            set 
+            {
+                if (value <= 0 )
+                {
+                    throw new ArgumentException("Price shuld be positive");
+                }
+                this.price = value;
+            } 
+        
+        }
+        public FoodStorageType Type { get; set; }
+
+        public long StorageID 
+        {
+            get 
+            { 
+                return this.storageID; 
+            }
+            set 
+            {
+                if (value <=0)
+                {
+                    throw new ArgumentException("ID must be positive");
+                }
+                this.storageID = value;
+            } 
+        }
         // Methods
         public void Order(Food f)
         {
