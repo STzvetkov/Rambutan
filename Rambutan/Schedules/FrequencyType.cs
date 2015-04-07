3eced63213fd4e96f48bc57b76f2a6dac9210a10
milkokochev <milkokochev@gmail.com>
2015-04-07 11:24:15 +0300

    Schedules/FeedingSchedule.cs - promeni

---
+++ Rambutan/Schedules/FrequencyType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo.Schedules
+{
+    public enum FrequencyType
+    {
+        Daily, 
+        Weekly, 
+        Once
+    }
+}
